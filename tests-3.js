4567fbd72ebf0f21c2543e6cdb26b1186857c6a8
Borsch451 <borschkal@gmail.com>
2021-11-11 13:17:18 +0300

    first commit

---
+++ tests-3.js
@@ -0,0 +1,28 @@
+var chai = require('chai');
+var testCase = require('mocha').describe;
+var chaiHttp = require('chai-http');
+var should = chai.should();
+
+chai.use(chaiHttp);
+chai.use(require('chai-like'));
+chai.use(require('chai-things'));
+
+
+/* 
+testing https://jsonplaceholder.typicode.com
+JSONPlaceholder is a free online REST service that you can use whenever you need some fake data.
+You can refer to the website for the API documentation and examples.
+*/
+
+// test example for GET	/posts
+testCase('/GET posts with only a userId', function(){
+      it('it should GET all the posts with userId=1', (done) => {
+        chai.request('https://jsonplaceholder.typicode.com')
+            .get('/posts?userId=1')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array').that.contains.something.like({userId:1});
+                done();
+            });
+      });
+  });
---
+++ tests-5.js
@@ -0,0 +1,27 @@
+var chai = require('chai');
+var testCase = require('mocha').describe;
+var chaiHttp = require('chai-http');
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+
+/* 
+testing https://jsonplaceholder.typicode.com
+JSONPlaceholder is a free online REST service that you can use whenever you need some fake data.
+You can refer to the website for the API documentation and examples.
+*/
+
+// test example for GET	/posts
+testCase('/GET posts with a filled userId and empty title', function(){
+      it('it should GET an empty array', (done) => {
+        chai.request('https://jsonplaceholder.typicode.com')
+            .get('/posts?userId=1&title=')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.should.have.length(0);
+                done();
+            });
+      });
+  });
